936cda9f708a8a217364cb8f9fd39c61febd8f83
Petr Pátek <petrpatekml@gmail.com>
2017-03-07 12:33:04 +0100

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,32 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+       return console.log('Unable to connetct to mongodb server')
+    }
+    console.log(' Connected to Mongno server')
+
+db.collection('Todos').insertOne({
+text: "Something to do",
+    completed: false,
+},(err, result) => {
+        if (err) {
+            return console.log('unable to insert')
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2))
+})
+db.collection('Users').insertOne({
+    name: "Petr",
+    age: 21 ,
+    location: 'Praha'
+}, (err, result) => {
+        if (err) {
+            console.log('Unable to write a log')
+        }
+        else {
+            console.log('adding note succesfully')
+        }
+})
+db.close();
+})
---
+++ playground/monog-find.js
@@ -0,0 +1,18 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
+    if (err) {
+        return console.log('Unable to connetct to mongodb server')
+    }
+    console.log(' Connected to Mongno server');
+db.collection('Todos').find().toArray().then((docs) => {
+        console.log('Todos')
+    console.log(JSON.stringify(docs,undefined, 2))
+
+})
+db.collection('Users').find({name: 'Petr'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2))
+})
+ //db.close();
+})
