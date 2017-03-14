dc0b02c3c258d2f2f52677755f6ffb3da71fe729
Petr Pátek <petrpatekml@gmail.com>
2017-03-14 11:02:42 +0100

    added user model for AUTH

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,32 +1,35 @@
-//const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
-    if (err) {
-       return console.log('Unable to connetct to mongodb server')
-    }
-    console.log(' Connected to Mongno server');
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-db.collection('Todos').insertOne({
-text: "Something to do",
-    completed: false,
-},(err, result) => {
-        if (err) {
-            return console.log('unable to insert')
-        }
-        console.log(JSON.stringify(result.ops, undefined, 2));
-})
-db.collection('Users').insertOne({
-    name: "Petr",
-    age: 21 ,
-    location: 'Praha'
-}, (err, result) => {
-        if (err) {
-            console.log('Unable to write a log')
-        }
-        else {
-            console.log('adding note succesfully')
-        };
-})
-db.close();
-})
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // Insert new doc into Users (name, age, location)
+  // db.collection('Users').insertOne({
+  //   name: 'Andrew',
+  //   age: 25,
+  //   location: 'Philadelphia'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
+
+  db.close();
+});
